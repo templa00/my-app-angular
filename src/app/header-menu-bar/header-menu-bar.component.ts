eefbf30a44cbe82deecd9a30643c36aec4fd4895
kurihara_hp_pc <kuriharahppc001@gmail.com>
2020-03-06 17:53:57 +0900

    ヘッダースクロール処理追加

--- src/app/header-menu-bar/header-menu-bar.component.ts
+++ src/app/header-menu-bar/header-menu-bar.component.ts
@@ -12,4 +12,10 @@ export class HeaderMenuBarComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollTo(className: string):void {
+    const elementList = document.querySelectorAll('.' + className);
+    const element = elementList[0] as HTMLElement;
+    element.scrollIntoView({ behavior: 'smooth' });
+ }
+
 }

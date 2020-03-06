255e917344e0e290c01f4489537543d00356f146
kurihara_hp_pc <kuriharahppc001@gmail.com>
2020-03-06 18:35:33 +0900

    スキルを定数可

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SKILLS } from './Skill';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  skills = SKILLS;
+  constr;uctor() { 
+  }
 
   ngOnInit() {
   }
---
+++ src/app/profile/skill.ts
@@ -0,0 +1,21 @@
+interface Skill {
+    name: string;
+    rate: string;
+  }
+
+export const SKILLS: Skill[] = [
+  { name: 'C#' , rate:'80%'},
+  { name: 'PHP' , rate:'50%'},
+  { name: 'HTML' , rate:'60%'},
+  { name: 'CSS' , rate:'60%'},
+  { name: 'JavaScripot' , rate:'60%'},
+  { name: 'TypeScript' , rate:'40%'},
+  { name: 'Objective-C' , rate:'60%'},
+  { name: 'Swift' , rate:'60%'},
+  { name: 'Java' , rate:'60%'},
+  { name: 'MySql' , rate:'60%'},
+  { name: 'AWS' , rate:'40%'},
+  { name: 'FireBase' , rate:'60%'},
+  { name: 'Unity' , rate:'80%'},
+  { name: 'Git' , rate:'80%'},
+];
